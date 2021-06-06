b9245a2c7fc2b54c160b75bcbcd6ab65b68eb221
Vidhi <thewarriorwizardgirl@gmail.com>
2021-06-06 21:29:12 +0530

    v1

---
+++ app/src/main/java/com/example/coffeeorderingapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.coffeeorderingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity=2;
+    int pricePerCup=30;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        int price=quantity*pricePerCup;
+        String priceMessage = "Price: Rs "+ price + "\n" + "Thank you!";
+        displayMessage(priceMessage);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+    public void increment(View view) {
+        quantity=quantity+1;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        quantity=quantity-1;
+        display(quantity);
+    }
+
+
+}
